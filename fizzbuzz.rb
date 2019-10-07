fbf127b182fe49cca11a76fe54602c4cd6a81594
Mark Kauffman <markkauffman22@gmail.com>
2019-10-07 15:54:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,22 +1,23 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(i)
   puts "In fizzbuzz ..................."
   i = 1
-  out = ""
   while i <= 15
     puts i 
     if i % 15 == 0
       puts "FizzBuzz"
-      out = 
+      return = "FizzBuzz"
     elsif i % 3 == 0
       puts "Fizz"
+      return = "Fizz"
     elsif i % 5 == 0 
       puts "Buzz"
+      return = "Buzz"
     else
       puts "nil"
-      #return nil
+      return nil
     end 
     i += 1 
   end  
